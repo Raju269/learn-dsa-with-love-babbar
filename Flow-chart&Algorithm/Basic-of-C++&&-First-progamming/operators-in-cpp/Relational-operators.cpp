c3fd634ea1bb20b066a1c67f7c3c25b800c1f582
Raju kumar <raju631149@gmail.com>
2025-06-24 14:03:20 +0530

    Basic operation of Relation  opertion

---
+++ Flow-chart&Algorithm/Basic-of-C++&&-First-progamming/operators-in-cpp/Relational-operators.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main(){
+    // Relation operator ;
+    int x ,y ;
+    cout<<"Enter the value of  x is : ";
+    cin>>x;
+    cout<<"Enter the value of  y is : ";
+    cin>>y;
+    cout<< ( x < y )<<endl;
+    cout<< ( x > y )<<endl;
+    cout<< ( x <= y )<<endl;
+    cout<< ( x >= y )<<endl;
+    cout<< ( x == y )<<endl;
+    cout<< ( x != y )<<endl;
+
+    return  0;
+}

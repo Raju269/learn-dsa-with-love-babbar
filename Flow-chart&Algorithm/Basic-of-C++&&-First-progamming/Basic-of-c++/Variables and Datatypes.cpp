219d53a5b93d077922124bbcc43fa7013338d4cf
Raju kumar <raju631149@gmail.com>
2025-06-24 13:50:59 +0530

    Variable and Datatype related work

---
+++ Flow-chart&Algorithm/Basic-of-C++&&-First-progamming/Basic-of-c++/Variables and Datatypes.cpp
@@ -0,0 +1,84 @@
+#include<iostream>
+using namespace std;
+int main(){
+    // Variable define 
+    int age = 21;
+    cout<< "Raju's Age is "<< age << endl;
+
+    // decalartion means value is create but not initalize the 
+    int marks; 
+    marks = 23;
+    cout<< marks<<endl;  // if you not value inserted the give garbarize value 
+
+
+    // difinition 
+    int value = 21;
+    cout<< value <<endl;
+    
+    // manipulation or updation 
+    value =23;
+    cout<<value<<endl;
+
+    // int 
+    int count = 12;
+    cout<< "The value of count is : "<<count<<endl;
+
+    // Float
+    float pie =3.14;
+    cout<<"The value of pie is : "<<pie<<endl;
+
+    // char
+    char alaphabet = 'd';
+    cout<<"The value of char is : "<<alaphabet<<endl;
+
+    // Double 
+    double weight = 58.3434343;
+    cout<< "The value of weight is : "<<weight<<endl;
+
+    // Boolean ; 
+    bool isMale = 1;
+    bool isFemale = 0;
+    bool isChild = true;
+    bool isAdult = false;
+
+    cout<<"The value of isMale is "<<isMale<<endl;
+    cout<<"The value of isFemale is "<<isFemale<<endl;
+    cout<<"The value of isChild is "<<isChild<<endl;
+    cout<<"The value of isAdult is "<<isAdult<<endl;
+    int UserNumber ;
+    cout<<"Enter the value of Number  is : ";
+    cin>>UserNumber;
+    // Variable Naming conversation 
+    // Its should begin with an alphabet 
+    // They may be more than are alphabet but with out any space between them . 
+    // Digits may be used but only after alphabet . 
+    // No Special symbol can be used except the underscrore (_) symbols when multiple words are needed. an underscore should separate them. 
+    // No keyword or command can be used as a variable name . 
+    // All statement in C++ language are case sensitive . thus a variable A (in Upper case ) is considered different from a variable declared a in (Lower case . )
+    
+
+
+    // We used Camel case 
+    int areaOfRectanle ;
+    cout<<"Enter the value of number is : ";
+    cin>>areaOfRectanle;
+    return 0; 
+
+}
+
+//  Variables: 
+// Named storgage memory location 
+//   Dataypes : 
+// The datatypes specifies the size and type of information the variable will stored . 
+
+
+
+// #################### Homeworks sessions ########
+// why double integers show by double value 
+// How data is stored in memory in depth concept 
+// How data is getting stored in memory 
+// why smallest addresable bytes 
+// Decimal to binary and vice versa 
+// int main(){
+// single block ko variable declare nhi kar sakath but double scope mai kar sath hua kya reason ??? 
+// }

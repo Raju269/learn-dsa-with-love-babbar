4f84301f4e7179d6a53987e6d807b72943bb3bb1
Raju kumar <raju631149@gmail.com>
2025-06-24 13:33:06 +0530

    Added basic C++ practice file

---
+++ Flow-chart&Algorithm/Basic-of-C++&&-First-progamming/Basic-of-c++/basic-code-c++.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(){
+    cout<<"Namasta Dunia"<<endl;
+    // Input / output in C++ 
+    int n; 
+    cout<<"Ente the value on n is : "; //Output
+    cin>>n;//Input 
+    cout<<n;
+}

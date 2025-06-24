42739460a86b51898d460d42853beee3911d636a
Raju kumar <raju631149@gmail.com>
2025-06-24 13:50:17 +0530

    User Input value in c++

---
+++ Flow-chart&Algorithm/Basic-of-C++&&-First-progamming/Basic-of-c++/User Input.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main(){
+        int age; 
+        cout<< "Enter your age :"<<endl;
+        cin>>age;
+        cout<<"Your age is "<<age<<endl;
+}
+
+// cin input value hai 
+// Boolean cause if you are true and false they give zero value and if your provide 1 and 0 they give right value .
+// ################## HOme work #########
+// cin.ignore() 
+// cin.fail()
+// getline(cin, 10)
+// std::cout why they used or not using namespace std;
+// :: which operator 

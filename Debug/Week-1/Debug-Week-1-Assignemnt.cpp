833a3c46c05770b81a3e8e21723f0ae9b942dc0d
Raju kumar <raju631149@gmail.com>
2025-06-29 12:53:27 +0530

    solution

--- Debug/Week-1/Debug-Week-1-Assignemnt.cpp
+++ Debug/Week-1/Debug-Week-1-Assignemnt.cpp
@@ -28,3 +28,21 @@ int main() {
         cout<<-1;
     }
 }
+
+// Solution 
+// #include<iostream>
+// using namespace std;
+
+// int main() {
+// 	char c;
+//     cin>>c;
+//     if('a'<=c && c<= 'z'){
+//         cout<<0;
+//     }
+// 	else if('A'<=c && c<= 'Z'){
+//         cout<<1;
+//     }
+//     else{
+//         cout<<-1;
+//     }
+// }

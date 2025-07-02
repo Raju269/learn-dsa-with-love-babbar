20391d39f5134663ce49ddb99776cc0b666441b4
Raju kumar <raju631149@gmail.com>
2025-07-02 16:17:43 +0530

    Basic of array

---
+++ Array/Week-1/basic-of-array.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int arr[5] = {10,20,30,40,50};
+    for(int i=0;i<5;i++){
+        cout<<arr[i]<<endl;
+    }
+    int brr[10]={1,2,3,4,5,6,7,8,9,10};
+    for(int i=0;i<10;i++){
+        cout<<brr[i]<<endl;
+    }
+
+    
+    return 0;
+}
